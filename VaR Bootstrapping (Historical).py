28ef1bb10a9a28fc8cc717c8135209a471536ae1
Amogh Athalekar <66363222+amoghathalekar@users.noreply.github.com>
2020-07-11 12:29:12 +0530

    Update VaR Bootstrapping (Historical).py

--- VaR Bootstrapping (Historical).py
+++ VaR Bootstrapping (Historical).py
@@ -26,14 +26,14 @@ start = dt.datetime.today()-dt.timedelta(5500)
 end = dt.datetime.today()
 ohlcv_data = {}
 
-#Downloading NIFTY price data and calculating daily returns from September 2007 until now:
+# Downloading NIFTY price data and calculating daily returns from September 2007 until now:
 for ticker in stocks:
     ohlcv_data[ticker] = yf.download(ticker,start,end).dropna(how="all")
 
 nifty_ret = ohlcv_data["^NSEI"]["Adj Close"].pct_change(1).dropna()
 nifty_ret
 
-#Defining VaR Bootstrapping:
+# Defining VaR Bootstrapping:
 def var_bootstrap(returns, days, prob, I0, sims):
     
     days = int(days)
@@ -46,25 +46,25 @@ def var_bootstrap(returns, days, prob, I0, sims):
     return var
 
 
-#As per our problem:
+# As per our problem:
 I0 = 1000000     # ₹1M position
 sims = 1000000   # no.of simulations we are running
 prob = 1         # 1% VaR (probability)
 days = 63        # 1 quarter = 252 trading days/4 = 63 days 
 
 
-#Calculating VaR:
+# Calculating VaR:
 var_bootstrap(nifty_ret, days, prob, I0, sims)
 
 
 #########   Calculating and Visualizing the relationship between TIME PERIOD and VaR   ###########
 
-#Calculating the VaR for many time periods:
+# Calculating the VaR for many time periods:
 var_b1 = []
 for i in range(1, 252+1):
     var_b1.append(-var_bootstrap(nifty_ret, days = i, prob = 1, I0 = 1000000, sims = 10000))
 
-#Visualization:
+# Visualization:
 plt.figure(figsize = (4, 3))
 plt.plot(range(1, 252+1), var_b1)
 plt.title("1% VaR for ₹1,000,000", fontsize = 20)
@@ -75,12 +75,12 @@ plt.show()
 
 #########   Calculating and Visualizing the relationship between VaR and PROBABILITY  ###########
 
-#Calculating the VaR given probabilities between 0.1% and 5%:
+# Calculating the VaR given probabilities between 0.1% and 5%:
 var_b2 = []
 for i in np.linspace(0.1, 5, 100):
     var_b2.append(-var_bootstrap(nifty_ret, days = 63, prob = i, I0= 1000000, sims = 10000))
 
-#Visualization:
+# Visualization:
 plt.figure(figsize = (4, 3))
 plt.plot(np.linspace(0.1, 5, 100), var_b2)
 plt.title("Quarterly VaR for ₹1,000,000", fontsize = 20)
